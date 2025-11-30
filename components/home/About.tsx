1da6eb5fb68e449bb521f652ca9128f618a9a115
{uiabu} <ui.abukawsar@gmail.com>
2025-11-30 11:38:00 +0600

    Update About and Services components with new content and layout; add achievements image and remove outdated images

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 import Reveal from "../motion/Reveal";
 import Stagger from "../motion/Stagger";
+import { IoIosArrowForward, IoMdCheckboxOutline } from "react-icons/io";
 
 export default function About() {
   return (
-    <section className="bg-gray-50">
+    <section>
       <section className="max-w-[1640px] mx-auto md:py-16 py-8 px-8">
         <div className="grid lg:grid-cols-2 items-center gap-10 md:gap-6">
           {/* Left Image */}
@@ -25,65 +26,89 @@ export default function About() {
           <div className="flex flex-col">
             <Stagger>
               <Reveal tag="h2" y={16} opacityFrom={0}>
-                <h2 className="text-3xl md:text-4xl font-bold text-black mb-4">
+                <p className="text-base font-normal text-[#666666] mb-4">
                   About Us
+                </p>
+              </Reveal>
+              <Reveal tag="h2" y={16} opacityFrom={0}>
+                <h2 className="text-3xl md:text-4xl font-bold text-black mb-4">
+                  McCulloch Law, P.A.{" "}
+                  <p className="text-[#BA8E2D]  font-extrabold">
+                    Over 15 Years Of Experience{" "}
+                  </p>
                 </h2>
               </Reveal>
               <Reveal tag="p" y={12} opacityFrom={0.1}>
                 <p className="text-gray-600 text-sm md:text-base leading-relaxed mb-4">
-                  At Jachimek Wellness Center, we believe that the body has the
-                  ability to heal itself. Our goal is to remove any interference
-                  in the nervous system and allow the body to function at its
-                  best.
+                  McCulloch Law, P.A. proudly serves the Greater Tampa Bay Area,
+                  providing trusted legal representation in personal injury and
+                  criminal defense. Led by Attorney Drew McCulloch, a former
+                  state prosecutor with more than 15 years of experience, the
+                  firm is committed to protecting your rights, guiding you
+                  through complex legal matters, and helping you work toward the
+                  best possible outcome.
                 </p>
               </Reveal>
               <Reveal tag="p" y={12} opacityFrom={0.1}>
-                <p className="text-gray-600 text-sm md:text-base leading-relaxed mb-6">
-                  Stop by and meet our amazing team! We are here for you! Our
-                  friendly staff is dedicated to providing you with the best
-                  care and service to ensure your comfort throughout your
-                  journey. 
-                </p>
-              </Reveal>
+                <div className="grid grid-cols-2 gap-4 mt-4 mb-8 w-full">
+                  <ul className="space-y-3">
+                    <li className="flex items-center gap-2">
+                      <IoMdCheckboxOutline size={24} color="#BA8E2D" />
+                      Former State Prosecutor
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <IoMdCheckboxOutline size={24} color="#BA8E2D" />
+                      Experienced Trial Attorney
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <IoMdCheckboxOutline size={24} color="#BA8E2D" />
+                      Proven Case Results
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <IoMdCheckboxOutline size={24} color="#BA8E2D" />
+                      Personalized Legal Strategies
+                    </li>
+                  </ul>
 
-              {/* Explore More button */}
-              <Reveal y={10} opacityFrom={0}>
-                <Link
-                  href={"/team"}
-                  className="bg-black text-white px-6 py-3 rounded-full w-fit transition mb-8 hover:bg-white hover:border hover:border-black hover:text-black"
-                >
-                  Explore More
-                </Link>
-              </Reveal>
-
-              {/* Google Reviews */}
-              <Reveal y={10} opacityFrom={0}>
-                <div className="flex flex-col sm:flex-row sm:items-center gap-4 mt-6">
-                  <div className="flex items-center gap-2">
-                    <Image
-                      src="/images/about/google.png"
-                      alt="Google"
-                      width={1000}
-                      height={800}
-                      className="w-16"
-                    />
-                    <div className="flex text-yellow-400 text-lg">★★★★★</div>
-                  </div>
-                  <div className="text-black text-sm">
-                    <span className="font-semibold">4.9</span> | 140 Reviews
-                    <div>
-                      <Link
-                        href="https://share.google/fr5fqwEAfsaCT7WNX"
-                        className="text-primary underline hover:text-green-700"
-                      >
-                        View all Google Reviews here
-                      </Link>
-                    </div>
-                  </div>
+                  <ul className="space-y-3">
+                    <li className="flex items-center gap-2">
+                      <IoMdCheckboxOutline size={24} color="#BA8E2D" />
+                      24/7 Client Support
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <IoMdCheckboxOutline size={24} color="#BA8E2D" />
+                      Award-Winning Representation
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <IoMdCheckboxOutline size={24} color="#BA8E2D" />
+                      Free Consulting
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <IoMdCheckboxOutline size={24} color="#BA8E2D" />
+                      Strong Court Representation
+                    </li>
+                  </ul>
                 </div>
+              </Reveal>
+              <Reveal tag="p" y={12} opacityFrom={0.1}>
+                <Link
+                  href=""
+                  className="px-6 py-4 bg-[#BA8E2D] text-white inline-flex items-center gap-2 rounded-sm hover:bg-yellow-700 mt-4 "
+                >
+                  Free Consultation <IoIosArrowForward />
+                </Link>
               </Reveal>
             </Stagger>
           </div>
+        </div>
+        <div>
+          <Image
+            src="/images/about/achievements.png"
+            alt=""
+            width={1000}
+            height={80}
+            className="mx-auto mt-8"
+          />
         </div>
       </section>
     </section>
--- components/home/Services.tsx
+++ components/home/Services.tsx
@@ -4,79 +4,76 @@ import Image from "next/image";
 import Reveal from "../motion/Reveal";
 import Stagger from "../motion/Stagger";
 import Link from "next/link";
+import { FaBook } from "react-icons/fa6";
 
 const services = [
   {
-    title: "Back Pain",
+    title: "Drug Crimes",
     description:
-      "Relieve pain, restore mobility, and get back to your daily life.",
-    image: "/images/services/back-pain.png",
-    slug: "back-pain",
+      "Experienced defense for drug charges of all levels—protecting your rights, challenging evidence, and fighting for reduced penalties or dismissal.",
+    slug: "drug-crimes",
   },
   {
-    title: "Neck pain",
+    title: "Fraud",
     description:
-      "Customized recovery plans to help athletes return to peak performance safely.",
-    image: "/images/services/neck-pain.png",
-    slug: "neck-pain",
+      "Accused of fraud? We deliver strategic, detail-driven representation to defend your rights and safeguard your future.",
+    slug: "fraud",
   },
   {
-    title: "Spinal decompression",
+    title: "Injunctions",
     description:
-      "Non-surgical spinal care to ease pressure, reduce pain, and promote healing.",
-    image: "/images/services/spinal-decompression.png",
-    slug: "spinal-decompression",
+      "We help clients obtain or defend against injunctions, ensuring your safety, property, and legal rights are fully protected.",
+    slug: "injunctions",
   },
   {
-    title: "Car Accidents",
+    title: "Truck Accidents",
     description:
-      "Expert care for accident-related injuries to help you recover safely.",
-    image: "/images/services/car-accidents.png",
-    slug: "car-accidents",
+      "Truck accidents can cause serious injury. We guide you through claims, protect your rights, and pursue maximum recovery.",
+    slug: "truck-accidents",
   },
   {
-    title: "Sciatic Relief",
+    title: "Motorcycle Accidents",
     description:
-      "Target nerve pain at its source for lasting relief and improved function.",
-    image: "/images/services/sciatic-relief.png",
-    slug: "sciatic-relief",
+      "Motorcycle crashes can be life-changing. Our team fights for your rights, guiding you through claims and ensuring you get the justice you deserve.",
+    slug: "motorcycle-accidents",
   },
   {
-    title: "Headaches",
+    title: "Dog Bites",
     description:
-      "Alleviate tension and recurring headaches with personalized chiropractic care.",
-    image: "/images/services/headaches.png",
-    slug: "headaches",
+      "Dog bite injuries can be serious. Our experienced team fights to hold owners accountable and secure the recovery you deserve.",
+    slug: "dog-bites",
+  },
+  {
+    title: "Fatal Accidents",
+    description:
+      "When negligence causes an accidental death, we help families understand their rights, investigate the case, and pursue the compensation they deserve.",
+    slug: "fatal-accidents",
+  },
+  {
+    title: "Wrongful Death",
+    description:
+      "When a friend or family member passes away due to someone else’s negligence, our priority is to secure critical evidence, open a thorough investigation, and protect your family’s right to justice.",
+    slug: "wrongful-death",
   },
 ];
 
 export default function Services() {
   return (
-    <section className="w-full px-8 py-8 md:py-16">
+    <section className="w-full px-8 py-8 md:py-16 bg-[#F9F9F9]">
       <div className="max-w-[1640px] mx-auto">
         {/* Header */}
-        <div className="flex flex-col md:flex-row md:items-center md:justify-between mb-12">
+        <div className="flex flex-col items-center justify-center mb-12 max-w-3xl mx-auto">
           <Stagger>
-            <Reveal tag="div" y={16} opacityFrom={0}>
-              <div>
-                <h2 className="text-3xl font-bold text-gray-900">
-                  Our Services
-                </h2>
-                <p className="mt-2 text-gray-600 max-w-2xl">
-                  Our chiropractors offer various services, including
-                  chiropractic adjustments, spinal decompression therapy,
-                  rehabilitation exercises, and weight loss, to help our
-                  patients achieve optimal health.
-                </p>
-              </div>
+            <Reveal tag="h2" y={16} opacityFrom={0}>
+              <p className="text-base font-normal text-[#666666] mb-4">
+                Practice Areas
+              </p>
             </Reveal>
-            <Reveal y={12} opacityFrom={0}>
-              <Link
-                href="/services"
-                className="mt-6 md:mt-0 inline-flex items-center gap-2 border border-black rounded-full px-5 py-2  font-medium hover:text-white hover:bg-black transition text-black"
-              >
-                Explore More Services →
-              </Link>
+            <Reveal tag="h2" y={16} opacityFrom={0}>
+              <h2 className="text-3xl md:text-4xl font-bold text-black text-center ">
+                Explore the Services We Offer in Personal Injury and Criminal
+                Defense
+              </h2>
             </Reveal>
           </Stagger>
         </div>
@@ -86,19 +83,18 @@ export default function Services() {
           {services.map((service, index) => (
             <Reveal key={index} y={16} opacityFrom={0}>
               <Link href="/services" className="block h-full">
-                <div className="bg-white rounded-2xl shadow-sm hover:shadow-md transition p-6 flex flex-col items-center text-center border border-gray-100 h-full cursor-pointer hover:scale-105 transform duration-200">
-                  <Image
-                    src={service.image}
-                    alt={service.title}
-                    width={300}
-                    height={224}
-                    className="w-full h-auto object-cover object-top rounded-xl"
-                  />
-                  <div className="p-6 text-center">
-                    <h3 className="text-lg font-semibold text-gray-900">
+                <div className="group bg-white hover:bg-[#BA8E2D] duration-300 rounded-2xl shadow-sm hover:shadow-md transition p-6 lg:p-10 flex flex-col items-center text-center border border-gray-100 h-full cursor-pointer hover:scale-105 transform ">
+                  <div
+                    className="w-16 h-16 rounded-xl overflow-hidden flex justify-center items-center shadow-lg text-[#BA8E2D]
+                  group-hover:bg-white duration-300"
+                  >
+                    <FaBook size={24} />
+                  </div>
+                  <div className="pt-4 text-center">
+                    <h3 className="text-lg font-semibold text-gray-900 group-hover:text-white duration-300">
                       {service.title}
                     </h3>
-                    <p className="mt-2 text-sm text-gray-600">
+                    <p className="mt-2 text-sm text-gray-600 group-hover:text-white/90 duration-300">
                       {service.description}
                     </p>
                   </div>
