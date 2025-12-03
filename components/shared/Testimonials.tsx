ddaeac3aa999184c00a4698abf13d0f11f2ecb57
sajolbd <shamim15-9760@diu.edu.bd>
2025-12-03 17:04:29 +0600

    youtube video add

--- components/shared/Testimonials.tsx
+++ components/shared/Testimonials.tsx
@@ -63,7 +63,7 @@ export default function Testimonials() {
           <div className="relative w-full max-w-5xl mx-auto rounded-2xl overflow-hidden shadow-[0_10px_20px_rgba(0,0,0,0.1)]">
             <iframe
               className="w-full h-[450px] md:h-[550px] lg:h-[600px]"
-              src="https://www.youtube.com/embed/VIDEO_ID?rel=0"
+              src="https://www.youtube.com/embed/JVhIvwXv_G4?si=0_4MZWqPmEhliHN7"
               title="YouTube video"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
