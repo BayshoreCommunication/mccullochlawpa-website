893739b3fd0d2dceed17dd9d6464a0871eb95a43
sajolbd <shamim15-9760@diu.edu.bd>
2026-06-22 16:40:50 +0600

    Sitemap and robot.txt added

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/_next/",
+      disallow: ["/api/", "/private/"],
     },
     sitemap: "https://www.mcfloridalaw.com/sitemap.xml",
   };
